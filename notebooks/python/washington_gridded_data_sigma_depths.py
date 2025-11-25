d00238295563866c3278abfd4d7308dade865c03
Andrew Scherer <ascherer97@gmail.com>
2025-11-25 15:42:43 -0800

    adds calculations for t/s/spice/do along isopycnal surfaces

--- notebooks/python/washington_gridded_data_sigma_depths.py
+++ notebooks/python/washington_gridded_data_sigma_depths.py
@@ -16,7 +16,9 @@
 # %%
 from functools import partial
 from pathlib import Path
+from typing import cast
 
+import gsw
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
@@ -36,6 +38,18 @@ ce09 = ce09.swap_dims({"pressure": "depth"})
 # backfill surface data for identifying surface sigma_0, but limit to 5 m depth (10 data points at 0.5 m intervals)
 ce09_bfill = ce09.bfill("depth", limit=10)
 ce09["threshold_sigma_0"] = ce09_bfill["potential_density"].isel(depth=0) + MLD_THRESHOLD
+ce09["salinity_absolute"] = gsw.SA_from_SP(
+    ce09["salinity"],
+    ce09["pressure"],
+    ce09["longitude"],
+    ce09["latitude"],
+)
+ce09["conservative_temperature"] = gsw.CT_from_t(
+    ce09["salinity_absolute"],
+    ce09["temperature"],
+    ce09["pressure"],
+)
+ce09["spice"] = gsw.spiciness0(ce09["salinity_absolute"], ce09["conservative_temperature"])
 
 
 # %%
@@ -56,8 +70,8 @@ def calculate_mld(depth: np.ndarray, z: np.ndarray, sigma_0: np.ndarray, surface
     return float(mld)
 
 
-mld = np.empty(ce09["time"].size) * np.nan
-sigma_layer_depth = np.empty(ce09["time"].size) * np.nan
+mld = np.full(ce09["time"].size, np.nan)
+sigma_layer_depth = np.full(ce09["time"].size, np.nan)
 
 for i, (sigma_0, surface_sigma_0) in enumerate(
     zip(
@@ -132,6 +146,85 @@ plt.savefig("../misc/wa_profiler_sigma_at_mld.png", dpi=300)
 
 
 # %% [markdown]
-# ## Comparison to Oceanic Pathways model
+# ## Time series along 25.8 and 26.0
 
 # %%
+SIGMA_LAYERS = [25.8, 26.0]
+
+
+# %%
+def calculate_mld(depth: np.ndarray, z: np.ndarray, sigma_0: np.ndarray, surface_sigma_0: float) -> float:
+    """Calculate mixed layer depth for a single time step.
+
+    Args:
+        depth (np.ndarray): Array of depths at which to calculate the mixed layer depth.
+        z (np.ndarray): Array of depths corresponding to sigma_0.
+        sigma_0 (np.ndarray): Array of sigma_0 values.
+        surface_sigma_0 (float): Surface sigma_0 value.
+
+    Returns:
+        float: Mixed layer depth.
+
+    """
+    mld = np.interp(depth, z, sigma_0, left=np.nan, right=np.nan) - surface_sigma_0
+    return float(mld)
+
+
+sigma_layers_depth_array = np.full((len(SIGMA_LAYERS), ce09["time"].size), np.nan)
+
+for i, sigma_0 in enumerate(
+    ce09["potential_density"].isel(depth=slice(None)).to_numpy().T,
+):
+    z = ce09["depth"].to_numpy()[~np.isnan(sigma_0)]
+    sigma_0_filtered = sigma_0[~np.isnan(sigma_0)]
+    for j, sigma_layer in enumerate(SIGMA_LAYERS):
+        try:
+            sigma_layers_depth_array[j, i] = find_root(
+                partial(
+                    calculate_mld,
+                    z=z,
+                    sigma_0=sigma_0_filtered,
+                    surface_sigma_0=sigma_layer,
+                ),
+                np.min(z),
+                np.max(z),
+            )
+        except ValueError:
+            sigma_layers_depth_array[j, i] = np.nan
+
+sigma_layers_depth_da = xr.DataArray(
+    sigma_layers_depth_array,
+    dims=["sigma_layer", "time"],
+    coords={"sigma_layer": SIGMA_LAYERS, "time": ce09["time"]},
+)
+
+
+# %%
+oxy_interp = ce09["dissolved_oxygen"].interp(depth=sigma_layers_depth_da)
+temp_interp = ce09["conservative_temperature"].interp(depth=sigma_layers_depth_da)
+sal_interp = ce09["salinity"].interp(depth=sigma_layers_depth_da)
+spice_interp = ce09["spice"].interp(depth=sigma_layers_depth_da)
+
+# %%
+fig, axs = plt.subplots(3, 1, figsize=(8, 6), layout="constrained", sharex=True)
+ax0 = cast("plt.Axes", axs[0])
+ax1 = cast("plt.Axes", axs[1])
+ax2 = cast("plt.Axes", axs[2])
+
+ax0.plot(ce09["time"], -sigma_layers_depth_da.sel(sigma_layer=25.8), label="25.8 $\\mathsf{kg/m^3}$", c="#4477AA")
+ax0.plot(ce09["time"], -sigma_layers_depth_da.sel(sigma_layer=26.0), label="26.0 $\\mathsf{kg/m^3}$", c="#CC6677")
+ax1.plot(ce09["time"], spice_interp.sel(sigma_layer=25.8), c="#4477AA")
+ax1.plot(ce09["time"], spice_interp.sel(sigma_layer=26.0), c="#CC6677")
+ax2.plot(ce09["time"], oxy_interp.sel(sigma_layer=25.8), c="#4477AA")
+ax2.plot(ce09["time"], oxy_interp.sel(sigma_layer=26.0), c="#CC6677")
+
+ax0.set_ylabel("z (m)")
+ax1.set_ylabel("Spice ($\\mathsf{kg/m^3}$)")
+ax2.set_ylabel("Oxygen ($\\mathsf{\\mu mol/kg}$)")
+
+ax0.legend(framealpha=1, ncols=2, borderpad=0.2)
+ax1.axhline(0, color="k", linestyle="-")
+
+fig.suptitle("Washington offshore profiler water properties on isopycnal surfaces")
+
+plt.savefig("../misc/wa_offshore_profiler_isopycnal_properties_timeseries.png", dpi=300)
